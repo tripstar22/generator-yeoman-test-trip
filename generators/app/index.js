b1ffcdb5e4310d0b8fc76525e344b2c8e04e0b2a
Trip Pruitt <trip.pruitt@gmail.com>
2015-10-02 13:28:16 -0400

    first commit

---
+++ generators/app/index.js
@@ -0,0 +1,65 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the premium ' + chalk.red('YeomanTestTrip') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someOption',
+      message: 'Hello, are you ready to harness the power of Yeoman?!',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
+      );
+      this.fs.copy(
+        this.templatePath('_html'),
+        this.destinationPath('_html')
+      );
+      this.fs.copy(
+        this.templatePath('_assets'),
+        this.destinationPath('_assets')
+      );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
